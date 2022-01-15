31e454085c2e2098ac198be3592cccb2eb9f99f2
Taha <taha@bluelogic.ae>
2022-01-15 11:04:25 +0400

    base Lottery contract created, base test function created

---
+++ contracts/Lottery.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifer: MIT
+
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+contract Lottery is Ownable{
+
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+
+    LOTTERY_STATE public lottery_state;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+    } 
+
+    function enter() public payable{
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256){
+        (, int256 price , , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint(price) * 10**10;
+        uint256 costToEnter = (usdEntryFee * 10 ** 18) / adjustedPrice;
+        
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(lottery_state == LOTTERY_STATE.CLOSED, "Can't start a new lottery yet!");
+
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public {}
+
+}
